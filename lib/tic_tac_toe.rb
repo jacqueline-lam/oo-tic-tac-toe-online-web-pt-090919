2aa88b371f2480b7ab50bbf7ad34ba4ce14ee86d
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-09 04:38:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -140,7 +140,7 @@ class TicTacToe
       turn
     end
     puts "Congratulations #{winner}!"
-    print "Cat's Game!" if draw?
+    puts "Cat's Game!" if draw?
 
     # must allow players to take turn
     # check if game is over after every turn
